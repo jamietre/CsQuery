9d5938059cdd6a8cdf35346990f783717e0a5f7a
jamietre <jamietre@gmail.com>
2012-07-06 07:05:53 -0400

    added basic interfaces (does not compile, wip)

---
+++ source/CsQuery/Engine/IPseudoSelector.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CsQuery.Engine
+{
+    public interface IPseudoSelector
+    {
+        /// <summary>
+        /// Gets or sets criteria (or parameter) data passed with the pseudoselector
+        /// </summary>
+
+        public string Criteria {get;set;}
+
+        /// <summary>
+        /// Gets or sets zero-based index of this object.T
+        /// </summary>
+
+        public int Index { get; set; }
+
+        public bool Matches(IDomObject obj); 
+    }
+}
