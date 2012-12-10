f62ed0733fc25c74c30cd836693bde272a71d2a8
jamietre <jamietre@gmail.com>
2012-12-09 21:23:43 -0500

    wip

---
+++ website/csquery.com/csquery.com/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CsQuerySite.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Index/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
