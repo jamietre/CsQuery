e9ecb8a61fc4eaef5a5cf2289580b24e50ef0141
jamietre <jamietre@gmail.com>
2013-01-02 08:04:27 -0500

    wip

---
+++ website/csquery.com/csquery.com/Controllers/BaseController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CsQuery.Mvc;
+
+namespace CsQuerySite.Controllers
+{
+    public class BaseController : CsQueryController
+    {
+
+        public virtual void Cq_End()
+        {
+            // get all scripts that aren't found in the header or footer
+            var scripts = Doc["script"].Not(Doc["#footer-scripts,head"].Find("script"));
+
+            var head = Doc["head"];
+            var foot = Doc["#footer-scripts"];
+
+            // move them to the right place
+
+            foreach (var script in scripts)
+            {
+                if (script.HasClass("head"))
+                {
+                    head.Append(script);
+                }
+                else
+                {
+                    foot.Append(script);
+                }
+            }
+
+            if (PageData != null)
+            {
+                var script = CQ.Create("<script />")
+                    .AddClass("page-data")
+                    .Attr("type", "application/json");
+                script.Append(CQ.ToJSON(PageData));
+                Doc["body"].Append(script);
+            }
+        }
+    }
+}
--- website/csquery.com/csquery.com/scripts/syntaxhighlighter/shBrushCSharp.js
+++ website/csquery.com/csquery.com/scripts/syntaxhighlighter/shBrushCSharp.js
@@ -1,10 +1,62 @@
-/*
- * JsMin
- * Javascript Compressor
- * http://www.crockford.com/
- * http://www.smallsharptools.com/
-*/
+/**
+ * SyntaxHighlighter
+ * http://alexgorbatchev.com/SyntaxHighlighter
+ *
+ * SyntaxHighlighter is donationware. If you are using it, please donate.
+ * http://alexgorbatchev.com/SyntaxHighlighter/donate.html
+ *
+ * @version
+ * 3.0.83 (July 02 2010)
+ * 
+ * @copyright
+ * Copyright (C) 2004-2010 Alex Gorbatchev.
+ *
+ * @license
+ * Dual licensed under the MIT and GPL licenses.
+ */
+; (function () {
+    // CommonJS
+    typeof (require) != 'undefined' ? SyntaxHighlighter = require('shCore').SyntaxHighlighter : null;
 
-dp.sh.Brushes.CSharp=function()
-{var keywords='abstract as base bool break byte case catch char checked class const '+'continue decimal default delegate do double else enum event explicit '+'extern false finally fixed float for foreach get goto if implicit in int '+'interface internal is lock long namespace new null object operator out '+'override params private protected public readonly ref return sbyte sealed set '+'short sizeof stackalloc static string struct switch this throw true try '+'typeof uint ulong unchecked unsafe ushort using virtual void while';this.regexList=[{regex:dp.sh.RegexLib.SingleLineCComments,css:'comment'},{regex:dp.sh.RegexLib.MultiLineCComments,css:'comment'},{regex:dp.sh.RegexLib.DoubleQuotedString,css:'string'},{regex:dp.sh.RegexLib.SingleQuotedString,css:'string'},{regex:new RegExp('^\\s*#.*','gm'),css:'preprocessor'},{regex:new RegExp(this.GetKeywords(keywords),'gm'),css:'keyword'}];this.CssClass='dp-c';this.Style='.dp-c .vars { color: #d00; }';}
-dp.sh.Brushes.CSharp.prototype=new dp.sh.Highlighter();dp.sh.Brushes.CSharp.Aliases=['c#','c-sharp','csharp'];
+    function Brush() {
+        var keywords = 'abstract as base bool break byte case catch char checked class const ' +
+						'continue decimal default delegate do double else enum event explicit ' +
+						'extern false finally fixed float for foreach get goto if implicit in int ' +
+						'interface internal is lock long namespace new null object operator out ' +
+						'override params private protected public readonly ref return sbyte sealed set ' +
+						'short sizeof stackalloc static string struct switch this throw true try ' +
+						'typeof uint ulong unchecked unsafe ushort using virtual void while';
+
+        function fixComments(match, regexInfo) {
+            var css = (match[0].indexOf("///") == 0)
+				? 'color1'
+				: 'comments'
+            ;
+
+            return [new SyntaxHighlighter.Match(match[0], match.index, css)];
+        }
+
+        this.regexList = [
+			{ regex: SyntaxHighlighter.regexLib.singleLineCComments, func: fixComments },		// one line comments
+			{ regex: SyntaxHighlighter.regexLib.multiLineCComments, css: 'comments' },			// multiline comments
+			{ regex: /@"(?:[^"]|"")*"/g, css: 'string' },			// @-quoted strings
+			{ regex: SyntaxHighlighter.regexLib.doubleQuotedString, css: 'string' },			// strings
+			{ regex: SyntaxHighlighter.regexLib.singleQuotedString, css: 'string' },			// strings
+			{ regex: /^\s*#.*/gm, css: 'preprocessor' },		// preprocessor tags like #region and #endregion
+			{ regex: new RegExp(this.getKeywords(keywords), 'gm'), css: 'keyword' },			// c# keyword
+			{ regex: /\bpartial(?=\s+(?:class|interface|struct)\b)/g, css: 'keyword' },			// contextual keyword: 'partial'
+			{ regex: /\byield(?=\s+(?:return|break)\b)/g, css: 'keyword' }			// contextual keyword: 'yield'
+        ];
+
+        this.forHtmlScript(SyntaxHighlighter.regexLib.aspScriptTags);
+    };
+
+    Brush.prototype = new SyntaxHighlighter.Highlighter();
+    Brush.aliases = ['c#', 'c-sharp', 'csharp'];
+
+    SyntaxHighlighter.brushes.CSharp = Brush;
+
+    // CommonJS
+    typeof (exports) != 'undefined' ? exports.Brush = Brush : null;
+})();
+
