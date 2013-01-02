6d55ccaa6187a403bd8d781063df1cbca19b630b
jamietre <jamietre@gmail.com>
2013-01-02 17:35:24 -0500

    Changes to build scripts

--- build/csquery.build.bat
+++ build/csquery.build.bat
@@ -7,9 +7,9 @@ rem create signed version first so unsigned spec is left behind
 .\ProcessNuspec\bin\release\ProcessNuspec.exe csquery.nuspec ..\source\CsQuery\Csquery.Nuspec -id CsQuery -version %version%
 
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild ..\source\csquery\source\csquery.csproj /p:SignAssembly=true /p:AssemblyOriginatorKeyFile=csquery.snk
-nuget pack ..\source\CsQuery\CsQuery.Signed.nuspec -Prop Configuration=Debug -Symbols -OutputDirectory "./NugetPackages"
+nuget pack ..\source\CsQuery\CsQuery.Signed.nuspec -Prop Configuration=Release -Symbols -OutputDirectory "./NugetPackages"
 
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild ..\source\csquery.sln
-nuget pack ..\source\CsQuery\CsQuery.nuspec -Prop Configuration=Debug -Symbols -OutputDirectory "./NugetPackages"
+nuget pack ..\source\CsQuery\CsQuery.nuspec -Prop Configuration=Release -Symbols -OutputDirectory "./NugetPackages"
 
 pause
--- build/csquery.mvc.build.bat
+++ build/csquery.mvc.build.bat
@@ -1,13 +1,13 @@
-set version="1.3.0"
+set version="1.3.0.4"
 mkdir NugetPackages
 
-.\ProcessNuspec\bin\release\ProcessNuspec.exe csquery.mvc.nuspec ..\source\CsQuery\Csquery.Mvc.Signed.nuspec -id CsQuery.Mvc.Signed -version %version%
-.\ProcessNuspec\bin\release\ProcessNuspec.exe csquery.mvc.nuspec ..\source\CsQuery\Csquery.Mvc.Nuspec -id CsQuery.Mvc -version %version%
+.\ProcessNuspec\bin\release\ProcessNuspec.exe csquery.mvc.nuspec ..\source\CsQuery.Mvc\Csquery.Mvc.Signed.nuspec -id CsQuery.Mvc.Signed -version %version%
+.\ProcessNuspec\bin\release\ProcessNuspec.exe csquery.mvc.nuspec ..\source\CsQuery.Mvc\Csquery.Mvc.nuspec -id CsQuery.Mvc -version %version%
 
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild ..\source\csquery\source\csquery.csproj /p:SignAssembly=true /p:AssemblyOriginatorKeyFile=csquery.snk
-nuget pack ..\source\CsQuery\CsQuery.Mvc.Signed.nuspec -Prop Configuration=Debug -Symbols -OutputDirectory "./NugetPackages"
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild ..\source\CsQuery.Mvc\csquery.mvc.csproj /p:Configuration=Release /p:SignAssembly=true /p:AssemblyOriginatorKeyFile=..\csquery\csquery.snk
+nuget pack ..\source\CsQuery.Mvc\CsQuery.Mvc.Signed.nuspec -Prop Configuration=Release -Symbols -OutputDirectory "./NugetPackages"
 
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild ..\source\csquery.sln
-nuget pack ..\source\CsQuery\CsQuery.Mvc.nuspec -Prop Configuration=Debug -Symbols -OutputDirectory "./NugetPackages"
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild ..\source\csquery.mvc.sln /p:Configuration=Release
+nuget pack ..\source\CsQuery.Mvc\CsQuery.Mvc.nuspec -Prop Configuration=Release -Symbols -OutputDirectory "./NugetPackages"
 
 pause
