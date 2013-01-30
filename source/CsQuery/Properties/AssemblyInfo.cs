a160554b9b2df23cf06e5527e564ac309180917b
jamietre <jamietre@gmail.com>
2013-01-30 17:29:22 -0500

    Update binaries

--- source/CsQuery/Properties/AssemblyInfo.cs
+++ source/CsQuery/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.3.3.245")]
+[assembly: AssemblyVersion("1.3.3.246")]
 //[assembly: AssemblyFileVersion("1.3.3")]
 
