d453d929081c33ce87ec3963585401d79edb93ca
jamietre <jamietre@gmail.com>
2013-04-25 11:47:56 -0400

    Update build

--- source/CsQuery/Properties/AssemblyInfo.cs
+++ source/CsQuery/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.3.5.96")]
+[assembly: AssemblyVersion("1.3.5.97")]
 //[assembly: AssemblyFileVersion("1.3.3")]
 
