a6d8d9d157a623599c7a76ac2e3edcd4e29d2d2a
Leo Chen <coel@users.noreply.github.com>
2015-12-15 14:32:17 +1100

    Fix and unit test for :first-child selector (Issue #208)

---
+++ source/CsQuery.Tests/Core/Selectors/CSS3/FirstChild.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using Assert = NUnit.Framework.Assert;
+using Description = NUnit.Framework.DescriptionAttribute;
+
+namespace CsQuery.Tests.Core.Selectors
+{
+    public partial class Css3: PseudoSelector
+    {
+        [Test, TestMethod]
+        public void FirstChild()
+        {
+            var res = Dom["span :first-child"];
+            Assert.AreEqual(4, res.Length);
+
+            Assert.AreEqual("profile-triangle", res[0].ClassName);
+            Assert.AreEqual("reputation-score", res[1].ClassName);
+            Assert.AreEqual("badge2", res[2].ClassName);
+            Assert.AreEqual("badge3", res[3].ClassName);
+        }
+    }
+}
